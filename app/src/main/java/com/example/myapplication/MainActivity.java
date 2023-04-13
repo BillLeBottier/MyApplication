ac6306a11b0ad92f4616f24f4a68d3ff27ae683d
robinvassaux <vassauxrob@gmail.com>
2023-04-13 10:11:30 +0200

    j'ai rajouté un commentaire pour test ca parle de ton daron

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+// ton gros daron
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
